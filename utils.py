a56c73cc29f24dd96e1239308e40770941caac70
Tirth Jain <jaintirth24@gmail.com>
2019-12-24 16:10:46 +0530

    Formatting

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 FIELDS = ('NAME', 'WEBSITE', 'TECHNOLOGIES')
 
 def pretty_print_results(results):
-    print("-" * 150)
+    print("-" * 215)
     print('{:^70s}{:^60s}{:^10s}'.format(*FIELDS))
-    print("-" * 150)
+    print("-" * 215)
     for org in results:
         print('{name:<70s}{website:<60s}{technologies}'.format(**org)) 
