70a92b53a1de03d6796024d3d90d119a46fab580
Tirth Jain <jaintirth24@gmail.com>
2019-12-24 16:55:35 +0530

    Improved UI

--- main.py
+++ main.py
@@ -4,12 +4,38 @@ from utils import *
 with open('orgs-19.json', 'r') as file:
     organizations = json.load(file)
 
-tech = input("Name of technology: ")
+while True:
+    print("\n")
+    print("Enter the name of a technology to search for (Enter 'exit' to exit)")
+    user_input = input(">")
+    if user_input == 'exit':
+        break
+    else:
+        if user_input:
+            tech_name = user_input
+        else:
+            tech_name = ""
 
-if tech:
-    results = [org for org in filter(lambda x: tech in x['technologies'], organizations)]
-else:
-    results = organizations
+    if tech_name:
+        results = [org for org in filter(lambda x: tech_name in x['technologies'], organizations)]
+    else:
+        results = organizations
 
-pretty_print_results(results)
+    pretty_print_results(results)
+
+    if results:
+        while True:
+            print("\n")
+            print("Enter the Sr No. of an organization to know more about it. Press Enter to exit")
+            try:
+                org_number = int(input(">"))
+                print_org_data(results[org_number])
+            except ValueError:
+                break
+
+    print("\n")
+    print("Search again? (yN)")
+    ans = input(">")
+    if not (ans[0] == 'y' or ans[0] == 'Y'):
+        break
 
--- utils.py
+++ utils.py
@@ -1,8 +1,15 @@
-FIELDS = ('NAME', 'WEBSITE', 'TECHNOLOGIES')
+_FIELDS = ('SR NO.', 'NAME', 'WEBSITE', 'TECHNOLOGIES')
 
 def pretty_print_results(results):
     print("-" * 215)
-    print('{:^70s}{:^60s}{:^80s}'.format(*FIELDS))
+    print('{:^8s}{:^70s}{:^60s}{:^80s}'.format(*_FIELDS))
     print("-" * 215)
-    for org in results:
-        print('{name:<70s}{website:<60s}{technologies:<80s}'.format(**org)) 
+    for org in enumerate(results):
+        print('{num:<8}{name:<70s}{website:<60s}{technologies}'.format(num=org[0], **org[1])) 
+
+
+def print_org_data(org):
+    print(f"Name: {org['name']}\n")
+    print(f"Website: {org['website']}\n")
+    print(f"Technologies: {org['technologies']}\n")
+    print(f"Description: {org['description']}\n")
